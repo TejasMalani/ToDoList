d3472815b617c84e5cfa28588f3d469658588e0f
TejasMalani <63201614+TejasMalani@users.noreply.github.com>
2020-04-07 16:36:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('./dist/{{todoApp}}'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{todoApp}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/src/{{todoApp}}/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
